47c0918dfd53c01da1bbb8d56c9a84a15539deeb
dda410 <dimitri1215@gmail.com>
2016-12-03 20:09:04 +0100

    script that calculates how much the actual result differs from the correct one

---
+++ percentageFromCorrectness.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+programToExecute="$1"
+input="$2"
+expectedOutput=$(echo "${input::-2}OUT" | xargs -I 'file' cat 'file' | head -1)
+# java "$programToExecute" "$input" | tail -2 | head -1
+realOutput=$(java "$programToExecute" "$input" | tail -2 | head -1)
+echo "This is expected output: "
+echo "$expectedOutput"
+echo "this is real output: "
+echo "$realOutput"
+outputDifference=$(echo "($realOutput-$expectedOutput) * 100 / $expectedOutput" | bc -l)
+echo "The solution found by the program is $outputDifference% more expensive than the correct one"
+
