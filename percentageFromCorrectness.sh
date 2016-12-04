af74fbc4e69ac23852707bb4d272c48a7856d69a
dda410 <dimitri1215@gmail.com>
2016-12-04 14:57:40 +0100

    cleaned up

--- percentageFromCorrectness.sh
+++ percentageFromCorrectness.sh
@@ -2,12 +2,10 @@
 programToExecute="$1"
 input="$2"
 expectedOutput=$(echo "${input::-2}OUT" | xargs -I 'file' cat 'file' | head -1)
-# java "$programToExecute" "$input" | tail -2 | head -1
 realOutput=$(java "$programToExecute" "$input" | tail -2 | head -1)
-echo "This is expected output: "
+echo "This is the expected correct output: "
 echo "$expectedOutput"
-echo "this is real output: "
+echo "this is the obtained output: "
 echo "$realOutput"
 outputDifference=$(echo "($realOutput-$expectedOutput) * 100 / $expectedOutput" | bc -l)
 echo "The solution found by the program is $outputDifference% more expensive than the correct one"
-
