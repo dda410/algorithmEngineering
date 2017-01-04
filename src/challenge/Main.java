60866898bb5d036aaf9038ef50f962a5a8d7291e
dda410 <dimitri1215@gmail.com>
2017-01-04 19:11:25 +0100

    updated zip package containing sources files

--- src/challenge/Main.java
+++ src/challenge/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class Main {
